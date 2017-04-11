594e94dd7650413d44552bff5b4c75dfa646f35d
Nemi <nemibanigo@gmail.com>
2017-04-11 16:33:29 +0100

    fetch current weather for city

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 var https = require("https");
 
 https.get("https://api.darksky.net/forecast/8698d6119223261a3e71ee01901501f3/37.8267,-122.4233", function(response){
-	console.log(response.statusCode);
+	var info = "";
+	response.on("data", function(chunk){
+		info += chunk;
+	});
+
+	response.on("end",function(){
+		if(response.statusCode === 200){
+			try{
+				var data = JSON.parse(info)
+				console.log("Weather is "+data.currently.summary+" in "+data.timezone+".")
+			}catch (error){
+				console.log("Sorry something went wrong")
+			}
+		}
+		else{
+			console.log("Sorry, something went wrong")
+		}
+	});
 });
